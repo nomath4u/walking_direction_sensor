274f5382212ec3bbb65b42a197af422da6a356e0
Chris Harper <nomath4u@gmail.com>
2018-03-11 21:36:21 -0700

    Working well. Dual sensors have a wide angle and sometimes the further one detects earlier, need to sink them into sockets in enclosure to ensure more directed detection

--- walking_direction_sensor.ino
+++ walking_direction_sensor.ino
@@ -5,13 +5,13 @@
 
 
 #define PIR1_PIN D3 //Closer to door
-#define PIR2_PIN D4 //Further from door
+#define PIR2_PIN D8 //Further from door
 
 void MQTT_connect(); //ESP bug 
 void send_direction(int);
 
-WiFiClientSecure client;
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 Adafruit_MQTT_Publish direct_sensor = Adafruit_MQTT_Publish(&mqtt, DIRECTION_FEED);
 
 enum{ IN, OUT };
@@ -22,6 +22,7 @@ void setup() {
   WiFi.mode(WIFI_STA); //Don't let it act as an access point
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
     delay(200);
   }
 }
@@ -31,9 +32,10 @@ void loop() {
   int pin2;
   static int previous_pin1 = 0;
   static int previous_pin2 = 0;
+  static bool cooldown = 0; //Shouldn't technically be needed but seems to help
   pinMode(LED_BUILTIN, OUTPUT);
   MQTT_connect();
- 
+  
   pin1 = digitalRead(PIR1_PIN);
   pin2 = digitalRead(PIR2_PIN);
 
@@ -41,24 +43,36 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
+
   }
   
   if(pin1 != previous_pin1){
 
-   if( pin1 && pin2){ //Just hit first sensor but secon was already on
+   if( pin1 && pin2 && !cooldown){ //Just hit first sensor but secon was already on
+      Serial.println("Sending IN");
       send_direction(IN);
+      cooldown = 1;
     }
   }
 
   if(pin2 != previous_pin2){
 
-   if( pin2 && pin1){ //Just Hit second sensor and first was already on
+   if( pin2 && pin1 && !cooldown){ //Just Hit second sensor and first was already on
+      Serial.println("Sending OUT");
       send_direction(OUT);
+      cooldown = 1;
    }    
   } 
+
+  if( !pin1 && !pin2 ){
+    cooldown = 0; //Both sensors off we are ready to trigger again
+  }
   previous_pin1 = pin1;
   previous_pin2 = pin2;
-  delay(500);
+
+  //Serial.print("PINS: ");
+  //Serial.print(pin1); Serial.println(pin2);
+  delay(50);
 }
 
 void send_direction(int d){
